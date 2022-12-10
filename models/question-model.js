a56de9cc8ebb0c24480b15ed594749d4e30a8e55
Kevin-Liu-01 <66856750+Kevin-Liu-01@users.noreply.github.com>
2022-12-09 20:36:13 -0500

    Fix file endings

--- models/question-model.js
+++ models/question-model.js
@@ -1,4 +1,4 @@
-const QuestionModelSchema = require("../schemas/question.schema.js");
+const QuestionModelSchema = require("../schemas/question-schema.js");
 
 /* Create */
 QuestionModelSchema.Displayall = function (req, res) {
