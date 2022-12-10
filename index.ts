97132f5c729bdd47752a61b8571753e8976e07d6
Kevin-Liu-01 <66856750+Kevin-Liu-01@users.noreply.github.com>
2022-12-09 20:51:33 -0500

    CORS fix

--- index.ts
+++ index.ts
@@ -10,6 +10,28 @@ app.use(require("./routes/user.js"));
 // get driver connection
 const dbo = require("./database/conn.js");
 
+const whitelist = ["*"];
+// Bypass CORS
+app.use((req, res, next) => {
+  const origin = req.get("referer");
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,Content-Type,Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+  }
+  // Pass to next layer of middleware
+  if (req.method === "OPTIONS") res.sendStatus(200);
+  else next();
+});
+
 app.listen(port, () => {
   // perform a database connection when server starts
   // dbo.connectToServer(function (err) {
